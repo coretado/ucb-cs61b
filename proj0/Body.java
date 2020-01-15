2f3c1eb694f6c2352396cb196355818fc6b782d1
peterxjs <41130062+peterxjs@users.noreply.github.com>
2020-01-14 22:04:54 -0800

    starting project0

---
+++ proj0/Body.java
@@ -0,0 +1,27 @@
+public class Body {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Body(double xP, double yP, double xV,
+                double yV, double M, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = M;
+        imgFileName = img;
+    }
+
+    public Body(Body b) {
+        xxPos = b.xxPos;
+        yyPos = b.yyPos;
+        xxVel = b.xxVel;
+        yyVel = b.yyVel;
+        mass = b.mass;
+        imgFileName = b.imgFileName;
+    }
+}
