ec924642e6c870c5482853f567d320b6dc928aee
peterxjs <41130062+peterxjs@users.noreply.github.com>
2020-10-06 21:20:11 -0700

    fix check style...dangit

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -4,12 +4,15 @@ import org.junit.Test;
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     /**
-     * The bug in this code is that Integer classes were being compared using a == instead of int primitives.
-     * Checking for == equality between two classes will inevitably fail because you are comparing
-     * them by reference instead of value. You could fix this code by changing the function signature,
-     * but that might break the code of other users of this library in this hypothetical situation. So the best
-     * solution might be to use use Integer.equals() instead of changing the function signature to (int a, int b).
-     * Why it always fails specifically at the value of 128, I have no idea.
+     * The bug in this code is that Integer classes were being compared using
+     * a == instead of int primitives. Checking for == equality between two
+     * classes will inevitably fail because you are comparing them by
+     * reference instead of value. You could fix this code by changing the
+     * function signature, but that might break the code of other users of
+     * this library in this hypothetical situation. So the best solution
+     * might be to use use Integer.equals() instead of changing the function
+     * signature to (int a, int b). Why it always fails specifically at the
+     * value of 128, I have no idea.
      */
     public static boolean isSameNumber(Integer a, Integer b) {
         return a.equals(b);
