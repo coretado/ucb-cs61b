f90f61e315a2e66417d9e3c26787771b360beec8
peterxjs <41130062+peterxjs@users.noreply.github.com>
2020-11-04 23:35:06 -0800

    starting first attempt at CA for room generation

--- proj2/byog/Core/Room.java
+++ proj2/byog/Core/Room.java
@@ -1,44 +1,141 @@
 package byog.Core;
 
 import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
 import edu.princeton.cs.algs4.ST;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Room {
     // these values can be adjusted to change the room generation
     private static final int ROOM_WIDTH = 8;
-    private static final int ROOM_HEIGHT = 3;
+    private static final int ROOM_HEIGHT = 8;
 
-
-    private ST<String, TETile> tiles;
+    private Map<String, Coordinate> blob;
     private Coordinate origin;
     private SeedGenerator seedGen;
+    private TETile[][] internalGrid;
     private int height;
     private int width;
 
     public Room(SeedGenerator seedGen, Coordinate origin) {
         this.seedGen = seedGen;
-        this.height = this.seedGen.genNumber(ROOM_HEIGHT);
-        this.width = this.seedGen.genNumber(ROOM_WIDTH);
-        this.origin = origin;
-        this.tiles = new ST<>();
+        this.height = this.seedGen.genDimensionNumber(ROOM_HEIGHT);
+        this.width = this.seedGen.genDimensionNumber(ROOM_WIDTH);
+        this.internalGrid = new TETile[this.width][this.width];
+        this.blob = new HashMap<>();
+    }
+
+    public Coordinate getCoordinate() {
+        return origin;
+    }
+
+    public void setCoordinate(int x, int y) {
+        origin = new Coordinate(x, y);
+    }
+
+    /** creates blobs and determines the largest room by using the helper methods below */
+    private void generateCA() {
+        // start room generation
+        initiateCA();
+        // smooth CA 5 times; this is indeed a triple nested loop, but problem set is small
+        for (int smooth = 0; smooth < 5; smooth += 1) {
+            smoothCA();
+        }
+        // TODO - DFS to find largest blob that was made
     }
 
     /**
-     * Should generate a room and assign its tiles to the Symbol Table
-     * - the current idea I have right now is to try to use Cellular Automata to randomly generate
-     * a room with tiles of floor and walls; then check each tile if it surrounded by a majority of wall tiles
-     * change it's type to a tile type of wall (if its a floor) and vice versa; one thing I am unsure of
-     * is how to "trim" the walls so that the wall thickness is only 1, or if I should even worry about that at all
-     *
-     * Another thing this generate room function will have to do is randomly create and attach a corridor to the
-     * room
+     * first generation of CA blobs using the following rule:
+     * i) 55% random chance of a tile being floor and 45% random chance of a tile being wall
      */
-    private void generateRoom() {
+    private void initiateCA() {
         for (int x = 0; x < width; x += 1) {
             for (int y = 0; y < height; y += 1) {
-                String tileKey = (origin.getX() + x) + "," + (origin.getY() + y);
-                tiles.put(tileKey, seedGen.genTile());
+                internalGrid[x][y] = seedGen.genTile();
             }
         }
+    }
+
+    /**
+     * "smoothing" of the cellular automata using the following rules:
+     * i) if a floor cell has fewer than four adjacent floor cells, it becomes a wall (8 direction)
+     * ii) if a wall cell has six or more adjacent floor cells, it becomes a floor (8 direction)
+     */
+    private void smoothCA() {
+        for (int x = 0; x < width; x += 1) {
+            for (int y = 0; y < height; y += 1) {
+                checkNeighbors(internalGrid[x][y], x, y);
+            }
+        }
+    }
+
+    /** check neighbor in all 8 directions to see if the tile in question has to change based off the rules of life */
+    private void checkNeighbors(TETile tile, int x, int y) {
+        if (tile.character() == '#') {
+            int cardinalValues = checkCardinal('·', x, y);
+            if (cardinalValues > 5) {
+                internalGrid[x][y] = Tileset.FLOOR;
+            }
+            return;
+        }
+        int cardinalValues = checkCardinal('·', x, y);
+        if (cardinalValues < 4) {
+            internalGrid[x][y] = Tileset.WALL;
+        }
+    }
+
+    /** this is going to look awful, I'm sorry */
+    private int checkCardinal(char tiletype, int x, int y) {
+        int count = 0;
+        // diagonal top left
+        if (internalGrid[wrapX(x - 1)][wrapY(y - 1)].character() == tiletype) {
+            count += 1;
+        }
+        // directly above
+        if (internalGrid[x][wrapY(y - 1)].character() == tiletype) {
+            count += 1;
+        }
+        // diagonal top right
+        if (internalGrid[wrapX(x + 1)][wrapY(y - 1)].character() == tiletype) {
+            count += 1;
+        }
+        // directly left
+        if (internalGrid[wrapX( x - 1)][y].character() == tiletype) {
+            count += 1;
+        }
+        // directly right
+        if (internalGrid[wrapX(x + 1)][y].character() == tiletype) {
+            count += 1;
+        }
+        // diagonal bottom left
+        if (internalGrid[wrapX(x - 1)][wrapY(y + 1)].character() == tiletype) {
+            count += 1;
+        }
+        // directly below
+        if (internalGrid[x][wrapY(y + 1)].character() == tiletype) {
+            count += 1;
+        }
+        // diagonal bottom right
+        if (internalGrid[x][wrapY(y + 1)].character() == tiletype) {
+            count += 1;
+        }
+        return count;
+    }
+
+    private int wrapX(int inc) {
+        return inc % width;
+    }
+
+    private int wrapY(int inc) {
+        return inc % height;
+    }
+
+    /**
+     * DFS recursive function to determine the largest blob created by CA
+     */
+    private void fill() {
+        // TODO - implement
     }
 }
--- proj2/byog/Core/SeedGenerator.java
+++ proj2/byog/Core/SeedGenerator.java
@@ -21,10 +21,22 @@ public class SeedGenerator {
         return Math.max(1, seedVal);
     }
 
+    /** guaranteed to at least return a value of 3 for cellular automata room construction */
+    public static int genDimensionNumber(int value) {
+        int seedVal = seedGen.nextInt(value);
+        return Math.max(3, seedVal);
+    }
+
+    /** return a value between 0-99; 0-14 spawns a corridor */
+    public static int genCorridorPercentage() {
+        int seedVal = seedGen.nextInt(100);
+        return Math.max(1, seedVal);
+    }
+
     /** generate tiles to be used */
     public TETile genTile() {
-        int tile = seedGen.nextInt(1);
-        if (tile == 0) {
+        int tile = seedGen.nextInt(100);
+        if (tile < 55) {
             return Tileset.FLOOR;
         } else {
             return Tileset.WALL;
