4d106b931859dcb4ba1344e5730bb4b0119a6fad
peterxjs <41130062+peterxjs@users.noreply.github.com>
2021-01-03 15:04:23 -0800

    got ComplexOomage to fail; the trick (I think ) is to make the parameter list only have the value 255, thus causing an overflow of 0 so every item is distributed to bucket 0

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -1,5 +1,7 @@
 package hw3.hash;
 
+import edu.princeton.cs.algs4.Complex;
+import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -33,18 +35,15 @@ public class TestComplexOomage {
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 
-    /* TODO: Create a list of Complex Oomages called deadlyList
-     * that shows the flaw in the hashCode function.
-     */
-    /*
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
-
-        // Your code here.
+        for (int i = 0; i < 10000; i += 1) {
+            deadlyList.add(ComplexOomage.deadlyComplexOomage());
+        }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
