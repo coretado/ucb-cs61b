ff71d2dc1e4599db5406f5341e137df110a0c1dd
peterxjs <41130062+peterxjs@users.noreply.github.com>
2021-05-06 18:09:13 -0700

    first checkpoint on Rasterer api

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -8,9 +8,12 @@ import java.util.Map;
  * not draw the output correctly.
  */
 public class Rasterer {
+    private double ROOT_DELTA_X;
+    private double ROOT_DELTA_Y;
 
     public Rasterer() {
-        // YOUR CODE HERE
+        this.ROOT_DELTA_X = MapServer.ROOT_LRLON - MapServer.ROOT_ULLON;
+        this.ROOT_DELTA_Y = MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT;
     }
 
     /**
@@ -42,11 +45,227 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+
+        // Catching corner cases
+        if (params == null) {
+            return this.badRequestObject(results);
+        }
+        if (this.hasBadCoordinates(params)) {
+            return this.badRequestObject(results);
+        }
+
+        // Making Map to store all res data
+        Map<String, Object> data = new HashMap<>();
+        // Determining depth
+        data.put("depth", this.calculateDepth(params));
+        // Determining render grid
+        this.calculateRenderGrid(params, data);
+        // Assigning data to results
+        this.assignParams(results, data, params);
+
         return results;
     }
 
+    /**
+     * Helper function used to create a bad request return object
+     *
+     * @param results Map object used as a return by the server
+     * @return A map of results with dummy values AND a specified FALSE query success output
+     */
+    private Map<String, Object> badRequestObject(Map<String, Object> results) {
+        results.put("render_grid", null);
+        results.put("raster_ul_lon", -1);
+        results.put("raster_ul_lat", -1);
+        results.put("raster_lr_lon", -1);
+        results.put("raster_lr_lat", -1);
+        results.put("depth", 0);
+        results.put("query_success", false);
+        return results;
+    }
+
+    /**
+     * Helper function used to catch edge cases of coordinate inputs. These bad inputs include:
+     * - if y1 is somehow less than y0
+     * - if x1 is somehow less than x0
+     * - if the coordinates provided do not generate a rectangular query box that intersects with the ROOT coordinates
+     *
+     * @param params Map object from server request containing BearMaps Request parameters
+     * @return Boolean specifying whether the BearMaps request violates the above conditions
+     */
+    private boolean hasBadCoordinates(Map<String, Double> params) {
+        Double PARAM_ULLON = params.get("ullon");
+        Double PARAM_LRLON = params.get("lrlon");
+        Double PARAM_ULLAT = params.get("ullat");
+        Double PARAM_LRLAT = params.get("lrlat");
+
+        if (PARAM_LRLAT < PARAM_ULLAT) {
+            return true;
+        }
+        if (PARAM_LRLON < PARAM_ULLON) {
+            return true;
+        }
+        return this.hasIntersectionWithRoot(PARAM_ULLON, PARAM_LRLON, PARAM_ULLAT, PARAM_LRLAT);
+    }
+
+    /**
+     * Helper function to determine if BearMaps request makes a rectangular query box that is contained within the ROOT
+     * coordinates
+     *
+     * @param PARAM_ULLON - upper left longitude of request
+     * @param PARAM_LRLON - lower right longitude of request
+     * @param PARAM_ULLAT - upper left latitude of request
+     * @param PARAM_LRLAT - lower left latitude of request
+     * @return Boolean specifying whether the BearMaps request does not intersect the ROOT coordinates
+     */
+    private boolean hasIntersectionWithRoot(
+            Double PARAM_ULLON,
+            Double PARAM_LRLON,
+            Double PARAM_ULLAT,
+            Double PARAM_LRLAT
+    ) {
+        // x3 > x2
+        if (PARAM_ULLON > MapServer.ROOT_LRLON) {
+            return false;
+        }
+        // x4 < x1
+        if (PARAM_LRLON < MapServer.ROOT_ULLON) {
+            return false;
+        }
+        // y3 > y2
+        if (PARAM_ULLAT > MapServer.ROOT_LRLAT) {
+            return false;
+        }
+        // y4 < y1
+        return !(PARAM_LRLAT < MapServer.ROOT_ULLAT);
+    }
+
+    /**
+     * Helper function to assign values to results Map. NOTE: Will automatically put query_success to true!
+     *
+     * @param results - Map object
+     * @param data - Helper object containing calculated Render Grid, rasterer Longitude and Latitude, and Depth
+     */
+    private void assignParams(Map<String, Object> results, Map<String, Object> data, Map<String, Double> params) {
+        results.put("render_grid", data.get("render_grid"));
+        results.put("raster_ul_lon", data.get("raster_ul_lon"));
+        results.put("raster_ul_lat", data.get("raster_ul_lat"));
+        results.put("raster_lr_lon", data.get("raster_ul_lon"));
+        results.put("raster_lr_lat", data.get("raster_ul_lat"));
+        results.put("width", params.get("width"));
+        results.put("height", params.get("height"));
+        results.put("depth", data.get("depth"));
+        results.put("query_success", true);
+    }
+
+    /**
+     * Helper function used to calculate the correct depth level needed for BearMaps image request
+     *
+     * @param params - BearMaps request object
+     *
+     * @return Integer specifying the BearMaps image depth level needed
+     */
+    private int calculateDepth(Map<String, Double> params) {
+        double paramLRLON = params.get("lrlon");
+        double paramULLON = params.get("ullon");
+        double width = params.get("width");
+        double paramDPP = (paramLRLON - paramULLON) / width;
+        int depth = 0;
+        while (depth < 10) {
+            double divider = Math.pow(2.0, depth);
+            double deltaX = depth == 0 ? 0.0 : this.ROOT_DELTA_X / divider;
+            double longDPP = ((MapServer.ROOT_ULLON + deltaX) + MapServer.ROOT_ULLON) / MapServer.TILE_SIZE;
+            if (longDPP <= paramDPP) {
+                break;
+            }
+            depth += 1;
+        }
+        return depth;
+    }
+
+    /**
+     * Helper function used to generate the String Matrix of filenames needed to render the BearMaps image request
+     *
+     * @param params - BearMaps request object
+     * @param data - Local object for response information
+     *
+     * @return String Matrix of filenames
+     */
+    private void calculateRenderGrid(Map<String, Double> params, Map<String, Object> data) {
+        this.calculateImageRange(params, data); // THIS FUNCTION WILL MUTATE "data"
+        this.generateRenderGrid(data); // THIS FUNCTION WILL MUTATE "data"
+    }
+
+    private void calculateImageRange(Map<String, Double> params, Map<String, Object> data) {
+        int k = (int) Math.pow(2, (Integer) data.get("depth"));
+        double latIncrement = this.ROOT_DELTA_Y / Math.pow((Integer) data.get("depth"), 2);
+        double longIncrement = this.ROOT_DELTA_X / Math.pow((Integer) data.get("depth"), 2);
+        double ULLON = params.get("ullon");
+        double ULLAT = params.get("ullat");
+        double LRLON = params.get("lrlon");
+        double LRLAT = params.get("lrlat");
+        double mutativeULLON = MapServer.ROOT_ULLON;
+        double mutativeULLAT = MapServer.ROOT_ULLAT;
+
+        // finding latitude data
+        if (ULLAT < MapServer.ROOT_ULLAT) {
+            data.put("rastererULLAT", MapServer.ROOT_ULLAT);
+            data.put("y0", 0);
+        }
+        if (LRLAT < MapServer.ROOT_LRLAT) {
+            data.put("rastererLRLAT", MapServer.ROOT_LRLON);
+            data.put("y1", k - 1);
+        }
+        if (!data.containsKey("y0") && !data.containsKey("y1")) {
+            for (int i = 0; i < k; i += 1) {
+                if (ULLAT >= mutativeULLAT && ULLAT <= (mutativeULLAT + latIncrement)) {
+                    data.put("raster_ul_lat", mutativeULLAT);
+                    data.put("y0", i);
+                }
+                if (LRLAT <= (mutativeULLAT += latIncrement)) {
+                    data.put("raster_lr_lat", mutativeULLAT);
+                    data.put("y1", i);
+                }
+            }
+        }
+
+        // finding longitude data
+        if (ULLON < MapServer.ROOT_ULLON) {
+            data.put("rastererULLON", MapServer.ROOT_ULLON);
+            data.put("x0", 0);
+        }
+        if (LRLON > MapServer.ROOT_LRLON) {
+            data.put("rastererLRLON", MapServer.ROOT_LRLON);
+            data.put("x1", k - 1);
+        }
+        if (!data.containsKey("x0") && !data.containsKey("x1")) {
+            for (int i = 0; i < k; i += 1) {
+                if (ULLON >= mutativeULLON && ULLON <= (mutativeULLON + longIncrement)) {
+                    data.put("raster_ul_lon", mutativeULLON);
+                    data.put("x0", i);
+                }
+                if (ULLAT <= (mutativeULLON += longIncrement)) {
+                    data.put("raster_lr_lon", mutativeULLON);
+                    data.put("x1", i);
+                }
+            }
+        }
+    }
+
+    private void generateRenderGrid(Map<String, Object> data) {
+        int x0 = (int) data.get("x0");
+        int x1 = (int) data.get("x1");
+        int y0 = (int) data.get("y0");
+        int y1 = (int) data.get("y1");
+        int depth = (int) data.get("depth");
+        String[][] renderGrid = new String[(y1 - y0) + 1][(x1 - x0) + 1];
+
+        for (int i = x0; i <= x1; i += 1) {
+            for (int ii = y0; ii <= y1; ii += 1) {
+                renderGrid[ii][i] = "d" + depth + "_x" + i + "_y" + ii + ".png";
+            }
+        }
+
+        data.put("render_grid", renderGrid);
+    }
 }
